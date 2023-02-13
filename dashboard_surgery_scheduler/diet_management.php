0f438bf93a4d42e8f5ef6f92ede3a960e9ed6bf7
Mooyoung404 <72974835+JohnAndrew-2020@users.noreply.github.com>
2023-02-13 21:13:51 +0800

    Update diet_management.php

--- dashboard_surgery_scheduler/diet_management.php
+++ dashboard_surgery_scheduler/diet_management.php
@@ -5,9 +5,7 @@
   if($_SESSION['status'] != 'admin') {
 
     echo "<script>
-
       window.history.back()
-
     </script>";
 
   }
@@ -89,8 +87,7 @@
 
 <body>
 
-
-
+ 
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
 
@@ -172,7 +169,6 @@
                 </button>
               </form>
             </li>
-			
 
           </ul><!-- End Profile Dropdown Items -->
         </li><!-- End Profile Nav -->
@@ -268,23 +264,44 @@
 </p></center>
 </div>
 
-<hr>
+
+<br>
 <br>
 <br>
 
 	   
 <form>
-    <h2>ID Form</h2>
-    <select id="category" name="category">
-        <option value="pre-surgery">Pre-Surgery</option>
-        <option value="post-surgery">Post-Surgery</option>
-    </select>
+
+<div class="module04" style="background-image: url('img/personal-info - Copy.png')">
+
+<i class="fas fa-flask"></i>
+<br><br><br>
+&nbsp;&nbsp;&nbsp;<a href="dietary_food_form.php" class="button button1">Pre-Surgery</a></button>
+</div>
+
+<br><br>
+
+<div class="module04" style="background-image: url('img/personal-info - Copy.png')">
+
+<i class="fas fa-flask"></i>
+<br><br><br>
+&nbsp;&nbsp;&nbsp;<a href="dietary_food_form.php" class="button button1">Post-Surgery</a></button>
+</div>
+
+<br><br>
+
+<div class="module04" style="background-image: url('img/personal-info - Copy.png')">
+
+<i class="fas fa-flask"></i>
+<br><br><br>
+&nbsp;&nbsp;&nbsp;<a href="dietary_food_form.php" class="button button1">Balance-Diet</a></button>
+</div>
+
+<br><br><br>
+
     </div>
         <br>
         <br>
-  <label for="patient-id">Please enter your patient/outpatient ID:</label><br>
-  <input type="text" id="patient-id" name="patient-id"><br><br>
-  <a href="food_category.php" class="button button1">Click Here</a></button>
 </form> 
 
 
@@ -293,8 +310,9 @@
 <br>
 <br>
 <br>
+<br>
 
- <hr>
+
 
 
   <!-- ======= Footer ======= -->
